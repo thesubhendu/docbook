abe70ee4f50b59a22475197df2e31a98fb1154f5
subhendu <bhattasubhendu@gmail.com>
2025-02-05 11:48:59 +0545

    made compact routes

--- routes/api.php
+++ routes/api.php
@@ -8,30 +8,20 @@ use App\Http\Controllers\SearchDoctorController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
-
 
 require __DIR__ . '/auth.php';
 
 Route::group(['middleware' => ['auth:sanctum']], function () {
     Route::middleware('can:doctor')->group(function () {
-        Route::get('doctors', [DoctorController::class, 'show']);
-        Route::post('doctors', [DoctorController::class, 'store']);
-        Route::put('doctors', [DoctorController::class, 'update']);
-        Route::delete('doctors', [DoctorController::class, 'destroy']);
+        Route::apiResource('doctors', DoctorController::class)->only(['show','store','update','destroy']);
     });
 
     Route::middleware('can:patient')->group(function () {
         Route::get('search-doctors', SearchDoctorController::class);
-
-        Route::get('patients', [PatientController::class, 'show']);
-        Route::post('patients', [PatientController::class, 'store']);
-        Route::put('patients', [PatientController::class, 'update']);
-        Route::delete('patients', [PatientController::class, 'destroy']);
-
+        Route::apiResource('patients', PatientController::class)->only(['show','store','update','destroy']);
         Route::post('book', BookAppointmentController::class);
     });
 
